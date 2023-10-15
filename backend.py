2f738dfb52098dfac6a43a587e94095e27d2fe0d
Daniel Parra <dparra1@ufl.edu>
2023-10-15 03:13:27 -0400

    handled edge cases and included other components

--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import openai
 import json
 import csv
+import ast
 from langchain.document_loaders import TextLoader, DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 
@@ -24,8 +25,8 @@ functions = [
                 "prod_type":
                 {
                     "type": "string",
-                    "enum": ["plans", "devices", "information"],
-                    "description": "The product inside the container",
+                    "enum": ["plans", "devices", "information", "help"],
+                    "description": "The service, product, or assistance inside the container the client needs",
                 },
             },
             "required": ["row_type", "prod_type"],
@@ -101,8 +102,22 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process():
-    print("before")
-    data = request.form['data']
+    #data = request.form['data']
+    data = request.json["messages"][-1]["content"]
+
+
+    return_json = {
+        "display_type": "",
+        "product_type": "",
+        "product_items": [],
+        "messages": [
+            {
+                "role": "user",
+                "content": f"{data}"
+            },
+        ]
+    }
+
     print(data)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0613",
@@ -112,12 +127,14 @@ def process():
         temperature=0
     )
     print(response)
+    return_json["messages"].append(response["choices"][0]["message"])
     response_data = json.loads(response["choices"][0]["message"]["function_call"]["arguments"])
 
+    return_json["display_type"] = response_data["row_type"]
     print(response_data["prod_type"])
+    return_json["product_type"] = response_data["prod_type"]
 
-    if response_data["prod_type"] == "devices":
-        print("devices")
+    if response_data["prod_type"] == "devices": # DEVICES
         headers = ""
         with open("basic.csv", 'r') as file:
             csv_reader = csv.reader(file)
@@ -133,12 +150,16 @@ def process():
                 functions= functions,
                 function_call={"name": "focus"}
             )
+            return_json["messages"].append(response["choices"][0]["message"])
+            print(response)
             response_data = json.loads(response["choices"][0]["message"]["function_call"]["arguments"])
 
             row_text = "Product, "
             for title in response_data["items"]:
                     if title == "Product":
                         continue
+                    if type(title) == None:
+                        break
                     row_text = row_text + title + ", "
             row_text = row_text + "\n"
             for row in csv_reader:
@@ -149,6 +170,8 @@ def process():
                 for title in response_data["items"]:
                     if title == "Product":
                         continue
+                    if type(row.get(title)) == "NoneType":
+                        break
                     row_as_string = row_as_string + row.get(title) + ", "
                 row_text = row_text + row_as_string + '\n'
             
@@ -156,14 +179,54 @@ def process():
 
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo-0613",
-                messages=[{"role": "user", "content": f"CSV data for available devices and their attributes:\n{row_text}\n\nrequest:\nfrom the CSV data provided, {data}. Place all device or devices in a parsable array and do not provide any context."}],
+                messages=[{"role": "user", "content": f"CSV data for available devices and their attributes:\n{row_text}\n\nrequest:\nfrom the CSV data provided, {data}. Place all device name or device names in a parsable array and do not provide any context."}],
                 # functions= functions,
                 # function_call={"name": "get_devices"}
             )
-            print(response)
-        return jsonify(result=response)
-    elif response_data["prod_type"] == "plans":
-        print("plans")
+            return_json["messages"].append(response["choices"][0]["message"])
+            print(response["choices"][0]["message"]["content"]) # Array of Phone names
+            try:
+                array_output = json.loads(response["choices"][0]["message"]["content"])
+                print(array_output)
+                with open('devices.json', 'r') as file:
+                    device_data = json.load(file)
+                    for device in array_output:
+                        temp_data = device_data[device]
+                        temp_data["product"] = device
+                        return_json["product_items"].append(temp_data)
+            except json.JSONDecodeError as e:
+                string = response["choices"][0]["message"]["content"]
+                if string[0] == '[\'' or string[0] == '[\"' and string[-1] == '\"]' or string[-1] == '\']':
+                    array_output = ast.literal_eval(response["choices"][0]["message"]["content"])
+                    with open('devices.json', 'r') as file:
+                        device_data = json.load(file)
+                        for device in array_output:
+                            temp_data = device_data[device]
+                            temp_data["product"] = device
+                            return_json["product_items"].append(temp_data)
+                else: # attempt comma seperation
+                    if string[0] == '[':
+                        string = string[:-1]
+                    if string[-1] == '[':
+                        string = string[1:]
+                    array_output = string.split(', ')
+                    if array_output[0] == string:
+                        array_output = string.split('\n')
+                    with open('devices.json', 'r') as file:
+                        device_data = json.load(file)
+                        for device in array_output:
+                            print(device)
+                            temp_data = device_data[device]
+                            temp_data["product"] = device
+                            return_json["product_items"].append(temp_data)
+
+
+
+            print(return_json)
+
+        return return_json
+    
+    elif response_data["prod_type"] == "plans": # CELL PHONE PLANS
         with open("plans.csv", 'r') as file:
             csv_reader = csv.reader(file)
             row_text = ""
@@ -178,10 +241,129 @@ def process():
                 # functions= functions,
                 # function_call={"name": "get_plans"}
             )
+            return_json["messages"].append(response["choices"][0]["message"])
             print(response)
-        return jsonify(result=response)
+            print(response["choices"][0]["message"]["content"])
+            try: 
+                array_output = json.loads(response["choices"][0]["message"]["content"])
+                print(array_output)
+                # TODO: CREATE JSON FOR FRONTEND
+                with open('plans.json', 'r') as file:
+                    plan_data = json.load(file)
+                    for plan in array_output:
+                        temp_data = plan_data[plan]
+                        temp_data["product"] = plan
+                        return_json["product_items"].append(temp_data)
+            except json.JSONDecodeError as e:
+                string = response["choices"][0]["message"]["content"]
+                if string[0] == '[\'' or string[0] == '[\"' and string[-1] == '\"]' or string[-1] == '\']':
+                    array_output = ast.literal_eval(response["choices"][0]["message"]["content"])
+                    with open('plans.json', 'r') as file:
+                        plan_data = json.load(file)
+                        for plan in array_output:
+                            temp_data = plan_data[plan]
+                            temp_data["product"] = plan
+                            return_json["product_items"].append(temp_data)
+                else: # attempt comma seperation
+                    if string[0] == '[':
+                        string = string[:-1]
+                    if string[-1] == '[':
+                        string = string[1:]
+                    array_output = string.split(', ')
+                    if array_output[0] == string:
+                        array_output = string.split('\n')
+                    with open('plans.json', 'r') as file:
+                        device_data = json.load(file)
+                        for device in array_output:
+                            temp_data = device_data[device]
+                            temp_data["product"] = device
+                            return_json["product_items"].append(temp_data)
+            print(response)
+        print(return_json)
+        return return_json
     else:
-        return jsonify(result=response)
+        # title: []
+        # content: []
+        # links: []
+
+        info_obj = {
+            "title" :"",
+            "content": "",
+            "links": []
+        }
+
+        # Create Title
+        response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-0613",
+                messages=[{"role": "user", "content": f"Given a customer with this concern:{data}, generate a title for Verizon's webpage that addresses that customer's concern"}],
+                # functions= functions,
+                # function_call={"name": "get_plans"}
+            )
+        print(response)
+        return_json["messages"].append(response["choices"][0]["message"])
+        info_obj["title"] = response["choices"][0]["message"]["content"]
+
+        with open('links.txt', 'r') as file:
+            # Read all lines and store them in a list
+            links_list = file.readlines()
+
+            # Create an empty string to store the links
+            links_string = ''
+
+            # Iterate through the list of links and append them to the string
+            for link in links_list:
+                # Strip any leading or trailing whitespace and add a newline character
+                links_string += link.strip() + ',\n'
+
+        # get links
+        response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-0613",
+                messages=[{"role": "user", "content": f"Available links:\n{links_string}\n\nrequest:\nfrom the series of links provided, create a parsable array with at most 3 links that would most likely have information that will answer this clients concern: {data}. Only provide the complete parsable array with no context."}],
+                # functions= functions,
+                # function_call={"name": "get_plans"}
+            )
+        print(response)
+        return_json["messages"].append(response["choices"][0]["message"])
+        array_string_temp = response["choices"][0]["message"]["content"]
+        if array_string_temp[-1] != ']' and array_string_temp[-1] != '\"':
+            array_string_temp = array_string_temp + '\"]'
+        elif array_string_temp[-1] != ']': 
+            array_string_temp = array_string_temp + ']'
+        print(array_string_temp)
+        try:
+            array_output = json.loads(array_string_temp)
+            print(array_output)
+            info_obj["links"] = array_output
+        except json.JSONDecodeError as e:
+            string = response["choices"][0]["message"]["content"]
+            if string[0] == '[\'' or string[0] == '[\"' and string[-1] == '\"]' or string[-1] == '\']':
+                array_output = ast.literal_eval(response["choices"][0]["message"]["content"])
+                info_obj["links"] = array_output
+            else: # attempt comma seperation
+                if string[0] == '[':
+                        string = string[:-1]
+                if string[-1] == '[':
+                    string = string[1:]
+                array_output = string.split(', ')
+                if array_output[0] == string:
+                    array_output = string.split('\n')
+                info_obj["links"] = array_output
+
+        # Generate content with GPT 4
+        response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=[{"role": "user", "content": f"You are an employee of Verizon.Given an array of links:{array_string_temp}, visit the links and compose valuable information in an easy to comprehend manner in response this clients concern: {data}."}],
+                # functions= functions,
+                # function_call={"name": "get_plans"}
+            )
+        print(response)
+        return_json["messages"].append(response["choices"][0]["message"])
+        info_obj["content"] = response["choices"][0]["message"]["content"]
+
+        # Provide links
+        return_json["product_items"].append(info_obj)
+        return_json["product_type"] = "information"
+        return return_json
 
 @app.route('/restrict', methods=['POST'])
 def restrict():
